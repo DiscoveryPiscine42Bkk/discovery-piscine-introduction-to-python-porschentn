0ed75dabf7c3de764b8342af6a3fa9cfc312be49
porschentn <natanonbtn123@gmail.com>
2025-06-18 22:50:28 +0700

    Update play_with_arrays.py

--- cell05/ex02/play_with_arrays.py
+++ cell05/ex02/play_with_arrays.py
@@ -1,1 +1,10 @@
+number_array = [2, 8, 9, 48, 8, -22, -12, -2]
 
+unique_array = list(set(number_array))
+
+positive_array = [x for x in unique_array if x > 0]
+
+positive_array.sort()
+
+print("Original array:", number_array)
+print("New array:", positive_array)
