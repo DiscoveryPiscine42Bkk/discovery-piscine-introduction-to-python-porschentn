8d2005aa3b2379af6f33ff65ef02090d3e0ced55
porschentn <natanonbtn123@gmail.com>
2025-06-19 06:54:15 +0000

    cell5

--- cell05/ex06/upset_it.py
+++ cell05/ex06/upset_it.py
@@ -1,1 +1,10 @@
+#!/usr/bin/env python3
+import sys
 
+def main():
+    if len(sys.argv) == 2:
+        # แปลงพารามิเตอร์ตัวที่ 1 เป็นตัวพิมพ์ใหญ่
+        print(sys.argv[1].upper())
+    else:
+        print("none")
+main()
